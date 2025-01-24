fe070a1f991834aa14c6e0dcd79e6e7c17071c0d
Soichiro Yamane <soichiro.yamane.19970619@gmail.com>
2025-01-24 22:59:23 +0900

    ♻️ (icon_updater.sh): add checks and error handling for tmp_icons directory and pnpm commands

--- sketchybar/icon_updater.sh
+++ sketchybar/icon_updater.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 
+# if tmp_icons exists, remove it
+if [ -d "tmp_icons" ]; then
+  echo "Removing existing tmp_icons directory..."
+  rm -rf tmp_icons
+fi
+
 git clone https://github.com/SoichiroYamane/sketchybar-app-font-bg tmp_icons
+
+cd ./tmp_icons || exit
+
+if pnpm install; then
+  echo "done: pnpm install"
+else
+  echo "pnpm install failed"
+fi
+
+if pnpm run build:install; then
+  echo "done: pnpm run build:install"
+  cd ..
+else
+  echo "pnpm build:install failed"
+  exit 1
+fi
 
 # replace ttf
 # move ./tmp_icons/public/dist/sketchybar-app-font-bg.ttf to $HOME/Library/Fonts/sketchybar-app-font-bg.ttf
